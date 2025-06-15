aeba5ad7ecda90a11b2029e6cf0ff1e3da0743d2
Zeynep <95102727+zeynepbasarann@users.noreply.github.com>
2025-06-15 21:12:01 +0300

    Update firebaseConfig.js

--- poseCareApp-master/firebaseConfig.js
+++ poseCareApp-master/firebaseConfig.js
@@ -1,15 +1,1 @@
-// firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore'; // Firestore kullanacaksan
-import { getAuth } from 'firebase/auth'; // Kullanýcý oturumu açma için
 
-
-
-// Firebase'i baţlat
-const app = initializeApp(firebaseConfig);
-
-// Firestore ve Auth instance'larýný al
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
